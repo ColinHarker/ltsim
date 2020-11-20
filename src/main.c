e13013fe0d1a3fde5110b69dbaa71a582cf0bea2
ColinHarker <Colinharker5@yahoo.com>
2020-11-20 12:30:31 -0500

    refactored c++ to c files

--- src/main.c
+++ src/main.c
@@ -16,20 +16,14 @@ int main()
 
   const int height = w.ws_row;
   const int width = w.ws_col;
-  int start_y = 0;
-  int start_x = 0;
+  //int start_y = 0;
+  //int start_x = 0;
 
   //WINDOW *win = newwin(height, width, start_y, start_x);
   WINDOW *disp = newwin(height / 2, width / 2, 0, 0);
   WINDOW *disp_2 = newwin(height, width / 2, width / 2, height);
 
   refresh();
-
-  int left, right, top, bottom, tlc, trc, blc, brc;
-
-  left = right = 0;
-  top = bottom = 0;
-  tlc = trc = blc = brc = 0;
 
   //wborder(win, left, right, top, bottom, tlc, trc, blc, brc);
   box(disp, 0, 0);
