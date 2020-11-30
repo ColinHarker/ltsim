8b731aa35c07f18817c0caf24e20bb2efb203f28
ColinHarker <Colinharker5@yahoo.com>
2020-11-30 16:45:27 -0500

    remove init.cpp

--- src/init.h
+++ src/init.h
@@ -3,6 +3,27 @@
 
 #include <ncurses.h>
 
-bool init();
+bool init()
+{
+
+    initscr();   // initialize the screen
+    noecho();    // keyboard input not registered to screen
+    curs_set(0); // curser invisible
+
+    if (!has_colors()) // check to see if the terminal in use supports colors
+    {
+        printw("Terminal does not support color");
+        getchar();
+        return false;
+    }
+
+    // initialize color pairs
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+
+    return true;
+}
 
 #endif
